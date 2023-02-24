717554d2572d569b01bb3cafd3e4525166a2f511
Gerardo Rodriguez <garc@outlook.com>
2023-02-23 23:44:54 -0700

    next.config.js updated and security policies removed to unblock scripts

--- lib/fpixel/FacebookPixel.tsx
+++ lib/fpixel/FacebookPixel.tsx
@@ -11,6 +11,7 @@ const FacebookPixel = () => {
   const router = useRouter();
   useEffect(() => {
     // This pageview only trigger first time (it is important for Pixel to have real information)
+    pageview();
     router.events.on('routeChangeComplete', handleRouteChange);
 
     // If the component is unmounted, unsubscribe
@@ -20,23 +21,23 @@ const FacebookPixel = () => {
   }, [router.events]);
 
   return (
-    <>
-  <Script id='facebook-pixel'>
-    {`
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', ${FB_PIXEL_ID});
-    fbq('track', 'PageView');
-    `}
-  </Script>
-    
-    </>
+  <Script id='facebook-pixel'
+    strategy='afterInteractive'
+    dangerouslySetInnerHTML={{
+      __html: `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', ${FB_PIXEL_ID});
+      fbq('track', 'PageView');
+      `
+    }}
+  />
 
   );
 };
--- next.config.js
+++ next.config.js
@@ -1,35 +1,35 @@
 /** @type {import('next').NextConfig} */
 
-const ContentSecurityPolicy = `
-  object-src 'none';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.googleapis.com https://*.gstatic.com *.google.com *.googleusercontent.com data: blob:;
-  img-src 'self' https://*.googleapis.com https://*.gstatic.com *.google.com  *.googleusercontent.com data:;
-  frame-src *.google.com;
-  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-  worker-src blob:;
-`;
+// const ContentSecurityPolicy = `
+//   object-src 'none';
+//   script-src 'self' 'unsafe-eval' 'unsafe-inline' https://connect.facebook.net/* https://*.googleapis.com https://*.gstatic.com *.google.com *.googleusercontent.com data: blob:;
+//   img-src 'self' https://connect.facebook.net/* https://*.googleapis.com https://*.gstatic.com *.google.com  *.googleusercontent.com data:;
+//   frame-src *.google.com;
+//   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+//   worker-src blob:;
+// `;
 
 // looqzzy1l3okfjeacigwhg
 
-const securityHeaders = [
-  {
-    key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
-  },
-];
+// const securityHeaders = [
+//   {
+//     key: 'Content-Security-Policy',
+//     value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+//   },
+// ];
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async headers() {
-    return [
-      {
-        // Apply these headers to all routes in your application.
-        source: '/:path*',
-        headers: securityHeaders,
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       // Apply these headers to all routes in your application.
+  //       source: '/(.*)',
+  //       headers: securityHeaders,
+  //     },
+  //   ];
+  // },
   images: {
     remotePatterns: [
       {
