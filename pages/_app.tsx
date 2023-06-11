04f27369199e9ae58958c2629959026e178bd72b
Gerardo Rodriguez <garc@outlook.com>
2023-06-11 14:22:57 -0600

    Adding Vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 
 import FacebookPixel from 'lib/fpixel/FacebookPixel';
 import GoogleAnalytics from 'lib/ga/GoogleAnalytics';
+import { Analytics } from '@vercel/analytics/react';
 
 import '../styles/globals.css';
 import { Loader } from '@components/ui';
@@ -19,6 +20,7 @@ export default function App({ Component, pageProps }: AppProps) {
           <GoogleAnalytics />
 
           <Component {...pageProps} />
+          <Analytics />
         </>
       )}
     </>
