af90ab8b18e01f5ac70fb92de6da9d67b5ddc656
Gerardo Rodriguez <garc@outlook.com>
2023-09-16 12:21:17 -0600

    Temporary AD was edited footer was moved to the header

--- components/ui/AdModal.tsx
+++ components/ui/AdModal.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import { useState } from 'react';
 import { Modal, Button } from 'react-bootstrap';
-import style from '@styles/Footer.module.css';
+// import style from '@styles/Footer.module.css';
 
 interface AdModalProps {
   showModal: boolean;
@@ -15,21 +15,39 @@ export const AdModal = ({ showModal, image, registerLink }: AdModalProps) => {
   const navigateToRegister = () => {
     window.open('http://' + registerLink, '_blank');
     setShowModal(false);
-    };
+  };
 
   return (
     <Modal
       size="lg"
+      // fullscreen
       show={show}
       key={image}
       centered
-    //   onHide={() => setShowModal(false)}
+      onHide={() => setShowModal(false)}
     >
       <Modal.Header
-        closeButton
-        style={{ backgroundColor: '#714b92' }}
-      ></Modal.Header>
-      <Modal.Body className='mx-auto'>
+        // closeButton
+        className='d-flex justify-content-end gap-2'
+        style={{ backgroundColor: '#FAFAFA' }}
+      >
+        <Button
+          variant="primary"
+          style={{ backgroundColor: '#714b92', borderColor: '#714b92' }}
+          onClick={navigateToRegister}
+        >
+          Register
+        </Button>
+        <Button
+          variant="primary"
+          className='me-2'
+          style={{ backgroundColor: '#d68eae', borderColor: '#d68eae' }}
+          onClick={() => setShowModal(false)}
+        >
+          Close
+        </Button>
+      </Modal.Header>
+      <Modal.Body className="d-flex flex-column align-items-center justify-content-center">
         <Image
           src={image}
           alt="Ad"
@@ -38,29 +56,23 @@ export const AdModal = ({ showModal, image, registerLink }: AdModalProps) => {
           className="img-fluid"
           priority={true}
           style={{ objectFit: 'contain' }}
-          
         />
-        <p className={`mt-4 text-center h3`}>
-              <span className='fw-bold' style={{color: '#714b92'}}>COST:</span> No charge. Light Lunch and Refreshments.
+        <p className={`mt-1 h4  align-self-start`}>
+          <span className="fw-bold" style={{ color: '#714b92' }}>
+            LOCATION:
+          </span>{' '}
+          45 Crosby Ave., Richmond Hill (The Community Church).
+        </p>
+        <p className={`mt-1 h4 align-self-start`}>
+          <span className="fw-bold" style={{ color: '#714b92' }}>
+            COST:
+          </span>{' '}
+          No charge. Light Lunch and Refreshments.
         </p>
       </Modal.Body>
-      <Modal.Footer style={{backgroundColor: '#FAFAFA'}}>
-          <Button
-            variant="primary"
-            style={{ backgroundColor: '#714b92', borderColor: '#714b92' }}
-            onClick={navigateToRegister}
-        
-            >
-            Register
-          </Button>
-          <Button
-            variant="primary"
-            style={{ backgroundColor: '#d68eae', borderColor: '#d68eae' }}
-            onClick={() => setShowModal(false)}
-            >
-            Close
-          </Button>
-        </Modal.Footer>
+      {/* <Modal.Footer style={{backgroundColor: '#FAFAFA'}}>
+     
+        </Modal.Footer> */}
     </Modal>
   );
 };
